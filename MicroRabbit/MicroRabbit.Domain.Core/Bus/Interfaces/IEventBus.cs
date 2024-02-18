5d1be39bc19654c7db8c15c33e8384d3da5f778b
Harilaos <haris.papamattheu@hotmail.com>
2024-02-18 22:26:29 +0200

    Domain structure

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/Interfaces/IEventBus.cs
@@ -0,0 +1,13 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus.Interfaces;
+
+public interface IEventBus
+{
+    Task SendCommand<T>(T command) where T : Command;
+
+    void Publish<T>(T @event) where T : Event;
+
+    void Subscribe<T, THandler>() where T : Event where THandler : IEventHandler<T>;
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,6 @@
+﻿namespace MicroRabbit.Domain.Core.Events;
+
+public abstract class Event
+{
+    public DateTime Timestamp { get; protected set; } = DateTime.Now;
+}
