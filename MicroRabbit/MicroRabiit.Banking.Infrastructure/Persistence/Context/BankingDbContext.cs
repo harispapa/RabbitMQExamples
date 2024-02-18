db4cd07791f378a84672d829e9331bfd6855cd0b
Harilaos <haris.papamattheu@hotmail.com>
2024-02-19 00:50:08 +0200

    Initiate Persistence Layer

---
+++ MicroRabbit/MicroRabiit.Banking.Infrastructure/Persistence/Context/BankingDbContext.cs
@@ -0,0 +1,9 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroRabbit.Banking.Infrastructure.Persistence.Context;
+
+public class BankingDbContext(DbContextOptions options) : DbContext(options)
+{
+	public DbSet<Account> Accounts { get; set; }
+};
---
+++ MicroRabbit/MicroRabiit.Banking.Infrastructure/Persistence/Repository/AccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Banking.Infrastructure.Persistence.Context;
+
+namespace MicroRabbit.Banking.Infrastructure.Persistence.Repository;
+
+public class AccountRepository(BankingDbContext dbContext) : IAccountRepository
+{
+	public IEnumerable<Account> GetAccounts() => 
+		dbContext.Accounts.ToList();
+}
