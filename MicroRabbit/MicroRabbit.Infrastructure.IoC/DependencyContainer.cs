d7625216b00a3e0ebcbf7bfa7130a385c827c666
Harilaos <haris.papamattheu@hotmail.com>
2024-02-18 23:40:45 +0200

    Initial commit of infrastructure Dependency project IoC

---
+++ MicroRabbit/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Domain.Core.Bus.Interfaces;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infrastructure.IoC;
+
+public class DependencyContainer
+{
+	public static void RegisterServices(IServiceCollection services)
+	{
+		// Domain Bus
+		services.AddTransient<IEventBus, RabbitMqBus>();
+	}
+}
