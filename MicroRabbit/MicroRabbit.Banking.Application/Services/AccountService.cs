8353b8896200870e583e32b69b8f906e7a6725b0
Harilaos <haris.papamattheu@hotmail.com>
2024-02-19 00:53:49 +0200

    Initiate Application Layer

---
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,11 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Application.Services;
+
+public class AccountService(IAccountRepository accountRepository) : IAccountService
+{
+	public IEnumerable<Account> GetAccounts() => 
+		accountRepository.GetAccounts();
+}
